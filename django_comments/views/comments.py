6570cfcd248c81a4834f86f635b739be07113812
jswv9@mst.edu <jswv9@mst.edu>
2015-02-05 13:16:23 -0600

    Update redirect params

--- django_comments/views/comments.py
+++ django_comments/views/comments.py
@@ -131,7 +131,7 @@ def post_comment(request, next=None, using=None):
     if request.user.groups.filter(name='Applicant').exists():
         return redirect("/application/submit")
     else:
-        return redirect("/applications/open/#{}".format(comment.content_object.user.username))
+        return redirect("/applications/open/?app={}".format(comment.content_object.user.username))
 
 comment_done = confirmation_view(
     template="comments/posted.html",
