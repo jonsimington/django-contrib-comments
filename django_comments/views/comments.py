0e5826c4c3f85a9e0d6422af6393eefd2dceb875
jswv9@mst.edu <jswv9@mst.edu>
2015-03-27 17:08:23 -0500

    convert entry id to str

--- django_comments/views/comments.py
+++ django_comments/views/comments.py
@@ -135,7 +135,7 @@ def post_comment(request, next=None, using=None):
     entry_id = comment.content_object.id
 
     # redirect to current page, open comments
-    return redirect(current_page + "?entry=" + entry_id)
+    return redirect(current_page + "?entry=" + str(entry_id))
 
 comment_done = confirmation_view(
     template="comments/posted.html",
